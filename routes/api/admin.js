1b43629d6f899149aed857926d143b310d0b743f
GAM <gam230596@gmail.com>
2017-12-24 14:13:57 +0700

    api user kcoin

---
+++ routes/api/admin.js
@@ -0,0 +1,114 @@
+var express = require('express');
+var mongodb=require('mongodb');
+var ObjectId=mongodb.ObjectId;
+var router = express.Router();
+
+/* GET students listing. */
+router.get('/', function(req, res, next) {
+    var MongoClient = mongodb.MongoClient;
+    var url = 'mongodb://NgoGam:gam23051996@ds163806.mlab.com:63806/userkcoin';
+    MongoClient.connect(url, function (err, db)
+    {
+        if(err)
+        {
+            console.log('Unable to connect to server',err);
+        }
+        else
+        {
+            console.log('Connection established to', url);
+
+            // Get the documents collection
+            var collection = db.collection('user');
+            collection.find({}).toArray(function (err, result) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.json(result);
+
+                }
+                //Close connection
+                db.close();
+            });
+        }
+    });
+});
+
+router.post('/', function(req, res,next) {
+    var MongoClient = mongodb.MongoClient;
+    var url = 'mongodb://NgoGam:gam23051996@ds163806.mlab.com:63806/userkcoin';
+    MongoClient.connect(url, function (err, db)
+    {
+        if(err)
+        {
+            console.log('Unable to connect to server',err);
+        }
+        else
+        {
+            console.log('Connection established to', url);
+            var myobj = { "email": req.body.email,"password":req.body.password,"firstname":req.body.firstname,"lastname":req.body.lastname};
+            db.collection("user").insertOne(myobj, function(err, result) {
+                if (err)
+                    res.send(err);
+                else
+                    res.send("1 documents inserted");
+                db.close();
+            });
+
+        }
+    });
+});
+
+router.delete('/:id', function(req, res, next) {
+    var MongoClient = mongodb.MongoClient;
+    var url = 'mongodb://NgoGam:gam23051996@ds163806.mlab.com:63806/userkcoin';
+    MongoClient.connect(url, function (err, db)
+    {
+
+        if(err)
+        {
+            console.log('Unable to connect to server',err);
+        }
+        else
+        {
+            console.log('Connection established to', url);
+
+            var myobj = { "_id": new ObjectId(""+req.params.id)};
+            db.collection("user").removeOne(myobj, function(err, result) {
+                if (err) res.send(err);
+                else
+                    res.send("1 document deleted");
+                db.close();
+            });
+
+        }
+
+    });
+});
+router.put('/:id', function(req, res, next) {
+    var MongoClient = mongodb.MongoClient;
+    var url = 'mongodb://NgoGam:gam23051996@ds163806.mlab.com:63806/userkcoin';
+    MongoClient.connect(url, function (err, db)
+    {
+        if(err)
+        {
+            console.log('Unable to connect to server',err);
+        }
+        else
+        {
+            console.log('Connection established to', url);
+
+            // Get the documents collection
+            var myobj = { "_id": new ObjectId(""+req.params.id)};
+
+            var newobj={"password":req.body.passwordupdate};
+            db.collection("user").updateOne(myobj, newobj, function(err, result) {
+                if (err) res.send(err);
+                else
+                    res.send("1 document updated");
+                db.close();
+            });
+
+        }
+    });
+});
+module.exports = router;
