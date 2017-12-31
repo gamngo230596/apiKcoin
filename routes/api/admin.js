51779f0fa9ac59a0e36bc6ad12760d285ba8b1a7
GAM <gam230596@gmail.com>
2017-12-31 13:24:13 +0700

    fix api put , post

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -39,6 +39,7 @@ router.post('/', function(req, res,next) {
     var address;
     var privatekey;
     var publickey;
+    console.log(req.body);
     MongoClient.connect(url, function (err, db)
     {
         if(err)
@@ -50,11 +51,10 @@ router.post('/', function(req, res,next) {
             console.log('Connection established to', url);
             axios.get('https://api.kcoin.club/generate-address')
                 .then(function(respone){
-                    console.log(respone.data);
                     address=respone.data.address;
                     privatekey=respone.data.privateKey;
                     publickey=respone.data.publicKey;
-                    var myobj = { "email": req.body.email,"password":req.body.password,"firstname":req.body.firstname,"lastname":req.body.lastname,"active":0,"actualbalance":0,"availablebalance":0,"address":address,"privatekey":privatekey,'publickey':publickey};
+                    var myobj = { "idwallet":req.body.idwallet,"email": req.body.email,"password":req.body.password,"firstname":req.body.firstname,"lastname":req.body.lastname,"active":0,"actualbalance":0,"availablebalance":0,"address":address,"privatekey":privatekey,'publickey':publickey};
                     db.collection("user").insertOne(myobj, function(err, result) {
                         if (err)
                             res.send(err);
@@ -112,6 +112,7 @@ router.put('/active/:id', function(req, res, next) {
             console.log('Connection established to', url);
 
             // Get the documents collection
+            console.log(req.body);
             var myobj = { "_id": new ObjectId(""+req.params.id)};
             var newobj={$set:{"active":1}};
             db.collection("user").updateOne(myobj, newobj, function(err, result) {
