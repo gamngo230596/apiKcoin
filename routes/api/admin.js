cddf5d3416fb4533ee692f5570387174b811a6fd
Tai Chau <tai.chaule@gmail.com>
2018-01-10 12:11:29 +0700

    update

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -98,6 +98,60 @@ router.get('/', function(req, res, next) {
     });
 });
 
+router.get('/transactionRev', function(req, res, next) {
+    var MongoClient = mongodb.MongoClient;
+    var url = 'mongodb://taichau:taichau123@ds249737.mlab.com:49737/kcoindeposits';
+    MongoClient.connect(url, function (err, db)
+    {
+        if(err)
+        {
+            console.log('Unable to connect to server',err);
+        }
+        else
+        {
+            console.log('Connection established to', url);
+
+            // Get the documents collection
+            var collection = db.collection('deposits');
+            collection.find({}).toArray(function (err, result) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.json(result);
+
+                }
+                //Close connection
+                db.close();
+            });
+        }
+    });
+});
+
+router.post('/transactionRev', function(req, res,next) {
+    var MongoClient = mongodb.MongoClient;
+    var url = 'mongodb://taichau:taichau123@ds249737.mlab.com:49737/kcoindeposits'; 
+    MongoClient.connect(url, function (err, db)
+    {
+        if(err)
+        {
+            console.log('Unable to connect to server',err);
+        }
+        else
+        {
+            console.log('Connection established to', url);
+            var myobj = { "idwallet":req.body.idwallet, "deposits":req.body.deposits};
+            db.collection("deposits").insertOne(myobj, function(err, result) {
+                if (err)
+                    res.send(err);
+                else
+                    res.send("1 documents inserted");
+                db.close();
+            });
+
+        }
+    });
+});
+
 router.post('/', function(req, res,next) {
     var MongoClient = mongodb.MongoClient;
     var url = 'mongodb://NgoGam:gam23051996@ds163806.mlab.com:63806/userkcoin';
